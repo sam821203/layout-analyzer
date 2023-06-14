6fe20f69abfbb0b6773d208879bb9c4543665820
sam <sam821203@yahoo.com.tw>
2023-06-14 15:46:31 +0800

    feat: push block 1 data in google sheet

--- app-google-sheet.py
+++ app-google-sheet.py
@@ -33,47 +33,27 @@ block_1_products = data["OnsaleJson"][0]["Nodes"]
 
 block_1_title = []
 block_1_desc = []
+block_1_final_price = []
+block_1_marketing_price = []
+block_1_url = []
 
 # 在 python 裡遍歷 list 不會拿到 1,2,3，而是第一個 dict，第二個 dict...
 for key in block_1_products:
-    block_1_title.append(key["Img"]["Title"])
-    block_1_desc.append(key["Link"]["Text1"])
-    # sheet_sheet01.update_values('A3', [['3'],['4'],['5'],['6']]) # 直的
+    block_1_title.append([key["Img"]["Title"]])
+    block_1_desc.append([key["Link"]["Text1"]])
+    block_1_final_price.append([key["Link"]["Text2"]])
+    block_1_marketing_price.append([key["Link"]["Text6"]])
+    block_1_url.append([key["Link"]["Url"]])
 
-# 创建 FormatBuilder 对象并设置字体样式
-format_builder = pygsheets.FormatBuilder()
-format_builder.set_font_bold(True)
+data = ["商品名稱", "商品描述", "商品特價", "商品原價", "商品連結"]
+columns = ['A', 'B', 'C', 'D', 'E']
 
-sheet_sheet01.update_value('A1', "品名")
-sheet_sheet01.update_value('B1', "品名描述")
-sheet_sheet01.update_value('C1', "價格")
-# sheet_sheet01.update_values('B1', [block_1_title])
-# sheet_sheet01.update_values('B2', [block_1_desc])
-# sheet_sheet01.update_values('A3', [['3'],['4'],['5'],['6']]) # 直的
+for i in range(len(data)):
+    cell = columns[i] + '1'
+    sheet_sheet01.update_value(cell, data[i])
 
-
-# def gsheet(self, stocks):
-#     # 由於Google擁有許多的雲端服務，所以需要先定義存取的Scope(範圍)，也就是Google Sheet(試算表)
-#     scopes = ["https://spreadsheets.google.com/feeds"]
-
-#     # 讀取Google Sheets API憑證(credentials.json)，並且傳入所要存取的Scope(範圍)，來進行驗證
-#     credentials = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scopes)
-
-#     # 進行Google Sheets API的授權
-#     client = gspread.authorize(credentials)
-
-#     # 傳入Google Sheet(試算表)的Key(金鑰)
-#     sheet = client.open_by_key("13Tl9G1kHYdPiRl0JBekaVnqgoUaxtbTb-o4hh01LWok").sheet1
-
-#     response = req.get("https://tw.stock.yahoo.com/q/q?s=2451")
-    
-#     data = bs4.BeautifulSoup(response.text, "lxml")
-
-#     tables = data.find_all('table')[2]
-#     ths = tables.find_all('th')[0:11]
-#     titles = ('資料日期',) + tuple(th.getText() for th in ths)
-#     sheet.insert_row(titles, 1)
-    
-#     # 將資料寫入Google Sheet
-#     for stock in stocks:
-#         sheet.append_row(stock)
+sheet_sheet01.update_values('A2', block_1_title)
+sheet_sheet01.update_values('B2', block_1_desc)
+sheet_sheet01.update_values('C2', block_1_final_price)
+sheet_sheet01.update_values('D2', block_1_marketing_price)
+sheet_sheet01.update_values('E2', block_1_url)
